e909a148f993da8df4dc31aec4ecfc9bfe4ecdfd
Josef Baltz <josef@orangeflare.me>
2020-07-17 03:51:04 -0500

    v1.1
    Added Support for Files

--- cmd/godecrypt/godecrypt.go
+++ cmd/godecrypt/godecrypt.go
@@ -6,31 +6,45 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func init(){
 	flag.StringVar(&text, "t", "", "Text to decrypt")
+	flag.StringVar(&input, "i", "", "File to decrypt")
 	flag.StringVar(&passphrase, "p", "", "")
 	flag.StringVar(&passphrase2, "p2", "", "")
 	flag.Parse()
-	if text == "" || passphrase == "" || passphrase2 == ""{
-		fmt.Println("[GoDecrypt] Please provide passphrase 1, passphrase 2, and text to be decrypted by adding '-t \"<text>\"' and '-p <pass>' and '-p2 <pass>'")
+	if (text == "" && input == "") || passphrase == "" || passphrase2 == "" {
+		fmt.Println("[GoDecrypt] Please provide passphrase 1, passphrase 2, and either text or file to be decrypted by adding '-t \"<text>\"' or -i <file location> and '-p <pass>' and '-p2 <pass>'")
 		os.Exit(0)
 	}
-	fmt.Println("[GoEncrypt] Version 2.0")
+	fmt.Println("[GoDecrypt] Version 1.1")
 }
 
-var text,passphrase,passphrase2 string
-var key []byte
+var text,input,passphrase,passphrase2 string
+var key,ciphertext []byte
+var err error
 
 func main(){
-	ciphertext, _ := hex.DecodeString(text) //Decode hexadecimal ciphertext into bytes
+	if input == "" {
+		ciphertext, _ = hex.DecodeString(text) //Decode hexadecimal ciphertext into bytes
+	} else {
+		ciphertext, err = ioutil.ReadFile(input)
+		if err != nil {
+			fmt.Println("[GoDecrypt] Error!\n"+err.Error())
+		}
+	}
 	key, _ := hex.DecodeString(passphrase) //Decode hexadecimal passphrase 1 into bytes
 	nonce, _ := hex.DecodeString(passphrase2) //Decode hexadecimal passphrase 2 into bytes
 	block, _ := aes.NewCipher(key) //Create a new cipher block with decoded passphrase 1
 	aesgcm, _ := cipher.NewGCM(block) //Things I don't fully understand yet
 	plaintext, _ := aesgcm.Open(nil, nonce, ciphertext, nil) //Things I don't fully understand yet part 2 electric boogaloo
-	fmt.Print("[GoDecrypt] Decrypted text: ") //Finally tell the user their decrypted text
-	fmt.Printf("%s\n", plaintext) //^
+	if input == "" {
+		fmt.Print("[GoDecrypt] Decrypted text: ") //Finally tell the user their decrypted text
+		fmt.Printf("%s\n", plaintext)             //^
+	} else {
+		ioutil.WriteFile("./output.dec", plaintext, 0644)
+	}
 }
--- cmd/goencrypt/goencrypt.go
+++ cmd/goencrypt/goencrypt.go
@@ -7,27 +7,43 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func init(){
 	flag.StringVar(&text, "t", "", "Text to encrypt")
+	flag.BoolVar(&output, "o", false, "Output results to file")
+	flag.StringVar(&input, "i", "", "Input file to encrypt")
 	flag.Parse()
-	if text == "" {
+	if text == ""  && input == "" {
 		fmt.Println("[GoEncrypt] Please provide text to be encrypted by adding '-t <text>'")
 		os.Exit(0)
 	}
-	fmt.Println("[GoEncrypt] Version 1.0")
+	if input != "" {
+		output = true
+	}
+	fmt.Println("[GoEncrypt] Version 1.1")
 }
 
-var text string
+var text, input string
+var output bool
+var plaintext []byte
+var err error
 
 func main(){
 	passbytes := make([]byte, 32) //Create array of 32 empty bytes
 	rand.Read(passbytes) //Randomize data in previously created array
 	passphrase := hex.EncodeToString(passbytes) //Encode the Bytes into a hexidecimal string
 	fmt.Println("[GoEncrypt] Your Passphrase 1 is: " + passphrase) //Tell the user what that string is, known as 'Passphrase 1'
-	plaintext := []byte(text) //Convert user provided plaintext into an array of bytes
+	if input == "" {
+		plaintext = []byte(text) //Convert user provided plaintext into an array of bytes
+	} else {
+		plaintext, err = ioutil.ReadFile(input)
+		if err != nil {
+			fmt.Println("[GoEncrypt] Error!\n"+err.Error())
+		}
+	}
 	block, _ := aes.NewCipher(passbytes) //Create a new cipher block with 'passphrase 1'
 	nonce := make([]byte, 12) //Create array of 12 empty bytes
 	rand.Read(nonce) //Randomize data in previously created array
@@ -35,6 +51,18 @@ func main(){
 	fmt.Println("[GoEncrypt] Your Passphrase 2 is: " + noncehex) //Tell the user what that string is, known as 'Passphrase 2'
 	aesgcm, _ := cipher.NewGCM(block) //Things I don't fully understand yet
 	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil) //Things I don't fully understand yet part 2 electric boogaloo
-	fmt.Print("[GoEncrypt] Encrypted text: ") //Finally tell the user their encrypted text
-	fmt.Printf("%x\n", ciphertext) //^
+	if input == "" {
+		fmt.Print("[GoEncrypt] Encrypted text: ") //Finally tell the user their encrypted text
+		fmt.Printf("%x\n", ciphertext)            //^
+	}
+	if output == false {
+		return
+	} else if input != "" {
+		outputBytes := []byte("Passphrase 1: "+passphrase+"\n\nPassphrase 2: "+noncehex+"\n\nOriginal File Location: "+input)
+		ioutil.WriteFile("./decrypt-keys.txt", outputBytes, 0644)
+		ioutil.WriteFile("./output.enc", ciphertext, 0644)
+	} else {
+		outputBytes := []byte("Passphrase 1: "+passphrase+"\n\nPassphrase 2: "+noncehex+"\n\nData: "+hex.EncodeToString(ciphertext))
+		ioutil.WriteFile("./output.txt", outputBytes, 0644)
+	}
 }
